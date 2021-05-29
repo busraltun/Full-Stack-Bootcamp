6118d86e72c37ba692a6d4df6ddc8b355cf14337
Busra Altun <busraltunb@gmail.com>
2021-05-29 11:14:06 +0300

    .Net e girmeden önce C# içerisindeki kapsülleme,get, set,  nesne, class, kütüphaneler konularına dair örnek çalışma yapıldı

---
+++ OOPOverview/OOPOverview/Film.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPOverview
+{
+    class Film
+    {
+        //ideamızı oluşturalım. public bilinen en kolay erişim belirtecidir.Nesneye ait nesneden erişilebilecek her şey public'tir.
+        private string name;
+        public string rating;
+        //public int price; //burada int yerine decimal kullanmak daha doğru olurdu fakat ben int veri tipini kullanmak istedim.
+        private int price;
+
+        /*Buraya kadar ideamızı oluşturmuş olduk. Fakat bu ideanın nesneleri Program çalıştığında alınacak. 
+        Yani şu kısma kadar henüz bir nesnemiz yok diyebiliriz. Yani filmin nesnesi halen yok. Ayrıca nesne 
+        oluştuğunda buradaki bu idealar bellekte heap kısmında yer alacak.
+        */
+
+        public void SetValuePrice(int price)
+        {
+            if (price < 0)
+            {
+                throw new Exception("Yaşını neden eksi değer olacak kadar küçük gösteriyorsun, tekrar dene !!");
+            }
+            this.price = price;
+        }
+
+        public int GetValuePrice()
+        {
+            return price;
+        }
+
+        //C# ta get set yöntemi bir arada aşağıdaki şekilde oluşturulur
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Filmin adını boş bıraktınız");
+                }
+                name = value;
+            }
+        }
+        public bool IsThereMovie { get; set; }
+        /* Burada set çalıştığı anda .NET arka planda RAM 'de primitive boolean tanımlıyor hemen o anda. Eğer setlemezsek tanımlamıyor.
+         */
+        public DateTime ReleaseDate { get; private set; } //sadece birini private yapabiliriz.
+                 
+      //set ile dışarıdan değer atama , get ile atandığı değer okunabilecek.
+                                                          
+      //Her Film nesnesinin ................. vardır. 
+
+    }
+}
---
+++ OOPOverview/OOPOverview/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace OOPOverview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Film ideasından türeyen bir şey yazıyoruz şimdi yani nesnemizi oluşturuyoruz. idea nın instance ı nesnedir. nesne = instance
+            Film comedyFilm = new Film(); //Film isimli ideadan comedyFilm adında bir nesne ürettik. Ve bu nesnenin adını, ıd sini, yayın tarihini değiştirebiliriz. comedyFilm nesnemiz bellekte stack de yer alır. Bir nesne bellekteki adresiyle tanımlanır.
+            comedyFilm.Name = "Ajan";
+            //comedyFilm.price = 500; SetValuePrice metodu sayesinde bunun yerine aşağıdaki satırı kullanırız.
+            comedyFilm.SetValuePrice(500); //ARTIK SET İŞLEMİNİ GERÇEKLEŞTİRMİŞ OLDUK. Yani denetimi arkaya bıraktık. İşte buna kapsülleme yani encapsullatıon denir.
+            comedyFilm.rating = "So comedy, great !"; //işte bu değerler artık heapte.
+            comedyFilm.IsThereMovie = true; //burada set çalıştığı anda ... cümlenin devamı film.cs içerisinden okuyalım
+
+            Film comedyFilm2 = comedyFilm;
+            comedyFilm2.Name = "Hayat Güzeldir ";
+            // comedyFilm2.price = 600; AssignValuePrice metodu sayesinde bunun yerine aşağıdaki satırı kullanırız.
+            comedyFilm2.SetValuePrice(600);
+            comedyFilm2.rating = "Thats amazing!";
+            Console.WriteLine(comedyFilm.Name);
+            Console.ReadLine();
+
+            /* 
+            Burada output olarak Hayat Güzeldir çıktısını alırız. Çünkü biz burada comedyFilm nesnesinin bellekteki
+            adresine gidip o adresi artık comedyFilm2 ye vermiş oluyoruz. Dolayısıyla o adresin heapte ulaştığı name, 
+            rating gibi ifadelerin değerlerini de değiştirdiğimizde çıktımız bu şekilde olmuş oldu.
+             */
+            //Yazdığım değer atamaların denetlenmesi önemlidir. Örneğin int Yas = -75 ; vermek her ne kadar olabilir gibi görünse de mantıken hatalıdır. Bir insanın yaşı - değer olamaz çünkü. İşte bu esnada devereye validatıon girer.
+            /*Console.WriteLine("Lütfen fiyat giriniz : ");
+            int priceValue= int.Parse(Console.ReadLine());
+            if(priceValue<0)
+            {
+                throw new Exception("Yaşını neden eksi değer olacak kadar küçük gösteriyorsun, tekrar dene !!");
+            }
+            comedyFilm2.price = priceValue;*/
+
+            /*Her seferinde bu hata denetimini bu şekilde yapmak güzel gibi görünse de yorucu ve eksik bir yaklaşımdır. Çünkü biz örneğin comedyFilm2 nin
+             instance nı alıp başka yerlerde başka fiyatlara atamayacak mıyız acaba sadece burada mı kullanıp denetleyeceğiz. Bu yukarıdaki yaklaşımla gidersek
+            bin tane comedyFilm2 instance mız olsa bin tane de if bloğumuz olacak demek olur e böyle bir şey mantıklı mı , değil. Bazı şeyler nesnelerin içerisinden
+            kontrol edilmelidir. O zaman fiyata biz bir varidasyon uygulayalım ve Film.cs de public olan belirtecini private olarak değiştirelim ki kimse kafasına göre
+            erişim sağlayamasın. private olarak değiştirdiğimizde ise şöyle bir durum ortaya çıkıyor Program.cs içerisinde price kullandığımız her yerde
+            price kelimesinin altı kırmızı çizgi ile uyarı veriyor. Dolayısıyla fiyata yani price a değer atamak için bir yöntem kullanmalıyız. Bu yöntemi de
+             Film.cs de public void SetValuePrice şeklinde oluşturalım. ARTIK SET İŞLEMİNİ GERÇEKLEŞTİRMİŞ OLDUK. Yani denetimi arkaya bıraktık. 
+            İşte buna kapsülleme yani encapsullatıon denir. Şunu da eklemek gerekir price a erişmek için giriş veç ıkışı sınırlandırdık. Metot kendi içerisinde
+            denetledi. Ama bizim fiyat (price) değerini dışarıdan okumamız gerekebilir. Mesela bu filmin fiyatı nedir diye sorduğumuzda price private olduğundan
+            dolayı erişemeyiz . Dolayısıyla price a erişemediğimize göre bize price 'ı döndüren bir şeye ihtiyacımız var. O zaman da Film.cs içerisine
+            public int GetValuePrice. Burada set,get yapısıyla ihtiyaç halinde validasyon uygulayacağımızı öğrenmiş olduk.
+             */
+            int comedyfilmPrice = comedyFilm2.GetValuePrice();
+
+
+        }
+    }
+}
